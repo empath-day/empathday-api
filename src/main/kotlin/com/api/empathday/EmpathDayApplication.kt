e77e0416e4bf5f1b4973fcd6bcd3d30853b6a2db
이재민 <rkwoals524@hanmail.net>
2023-07-19 22:52:02 +0900

    initialize commit

---
+++ src/main/kotlin/com/api/empathday/EmpathDayApplication.kt
@@ -0,0 +1,11 @@
+package com.api.empathday
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmpathDayApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmpathDayApplication>(*args)
+}
---
+++ src/test/kotlin/com/api/empathday/EmpathDayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.api.empathday
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmpathDayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
